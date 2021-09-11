9f2d3088fb63377a53c4e9d8150cd71745d3f89a
SilambarasanKv <silambu.kvs12@gmail.com>
2021-09-11 11:03:26 +0530

    solved back button issue

--- app/src/main/java/com/ahaguru/teacherahaguru/Fragments/MainFragment.java
+++ app/src/main/java/com/ahaguru/teacherahaguru/Fragments/MainFragment.java
@@ -1,18 +1,23 @@
 package com.ahaguru.teacherahaguru.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.ahaguru.teacherahaguru.MainActivity;
 import com.ahaguru.teacherahaguru.R;
@@ -22,6 +27,7 @@ public class MainFragment extends Fragment {
     NavController navController;
 
     Button btnSignup, btnManageTeachers;
+
 
     public MainFragment() {
         // Required empty public constructor
@@ -36,6 +42,19 @@ public class MainFragment extends Fragment {
 
         btnSignup = v.findViewById(R.id.btnSignup);
         btnManageTeachers = v.findViewById(R.id.btnManageTeachers);
+
+        v.setFocusableInTouchMode(true);
+        v.requestFocus();
+        v.setOnKeyListener( new View.OnKeyListener() {
+
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                getActivity().finish();
+
+                return true;
+            }
+        });
 
         return v;
     }
@@ -67,4 +86,6 @@ public class MainFragment extends Fragment {
             }
         });
     }
+
+
 }
--- app/src/main/java/com/ahaguru/teacherahaguru/MainActivity.java
+++ app/src/main/java/com/ahaguru/teacherahaguru/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
@@ -11,6 +12,8 @@ import androidx.navigation.ui.NavigationUI;
 import com.ahaguru.teacherahaguru.Fragments.MainFragment;
 import com.ahaguru.teacherahaguru.Room.TeacherRoomDatabase;
 import com.ahaguru.teacherahaguru.utils.FragmentStateSaver;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public FragmentStateSaver fragmentStateSaver;
@@ -43,4 +46,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onSupportNavigateUp();
     }
+
 }
